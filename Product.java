7784e94b6c1cae7960d2563c9acd48749c29c0cf
bhuvanaD <bhuvanadhanasekaran2013@gmail.com>
2016-08-12 15:41:45 +0600

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,33 @@
+package com.niit.shoppingcart.model;
+
+public class Product {
+	
+	private String id;
+	private String Name;
+	private String description;
+	private String price;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+}
